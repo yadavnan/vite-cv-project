b6e224e3f14f04c0f051d313e963fbe727302b12
yadavnan <yadavnan55@gmail.com>
2025-01-02 15:49:22 +0530

    previewrenderupdt

--- src/components/CVPreview.jsx
+++ src/components/CVPreview.jsx
@@ -6,27 +6,31 @@ const CVPreview = ({ generalInfo, education, workExperience }) => {
       <h2>Preview</h2>
       <div className="cv-section">
         <h4>General Information</h4>
-        <p>Name: {generalInfo.name}</p>
-        <p>Email: {generalInfo.email}</p>
-        <p>Phone: {generalInfo.phone}</p>
+        <p>Name: {generalInfo.name || "N/A"}</p>
+        <p>Email: {generalInfo.email || "N/A"}</p>
+        <p>Phone: {generalInfo.phone || "N/A"}</p>
       </div>
       <div className="cv-section">
         <h4>Education</h4>
-        <p>School: {education.schoolName}</p>
-        <p>Title of Study: {education.studyTitle}</p>
-        <p>Study Dates: {education.studyDate}</p>
+        <p>School: {education.schoolName || "N/A"}</p>
+        <p>Title of Study: {education.studyTitle || "N/A"}</p>
+        <p>Study Dates: {education.studyDate || "N/A"}</p>
       </div>
       <div className="cv-section">
         <h4>Work Experience</h4>
-        <p>Company: {workExperience.companyName}</p>
-        <p>Position: {workExperience.positionTitle}</p>
-        <p>Responsibilities: {workExperience.responsibilities}</p>
-        <p>Work Dates: {workExperience.workDates}</p>
+        <p>Company: {workExperience.companyName || "N/A"}</p>
+        <p>Position: {workExperience.positionTitle || "N/A"}</p>
+        <p>Responsibilities: {workExperience.responsibilities || "N/A"}</p>
+        <p>
+          Work Dates:{" "}
+          {workExperience.startDate && workExperience.endDate
+            ? `${workExperience.startDate} - ${workExperience.endDate}`
+            : "N/A"}
+        </p>
+        <p>Location: {`${workExperience.city || "N/A"}, ${workExperience.country || "N/A"}`}</p>
       </div>
     </div>
   );
 };
 
 export default CVPreview;
-
-
